7ad6526c088cefdf49af7dd9e23013ac96991404
Ty-Miki <ethioadvance@gmail.com>
2024-09-21 23:58:59 +0300

    First commit

---
+++ app/hooks/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'hooks'
+
+urlpatterns = [
+        path('upload/', views.upload_hook, name='upload'),
+        path('processing/<str:task_id>/', views.processing, name='processing'),
+        path('check_status/<str:task_id>/', views.check_task_status, name='check_status'),
+        path('download_zip/<str:task_id>/', views.download_zip, name='download_zip'),
+        path('download_output/<path:videopath>/', views.download_video, name='download_output'),
+        path('processing_successful/<str:task_id>/', views.processing_successful, name='processing_successful'),
+
+        
+        
+]
